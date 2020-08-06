14e6133343f75044c7b378303ecf81789ef43aa5
Abby Bowerman <abbybowerman@outlook.com>
2020-08-06 19:43:43 -0400

    Added start and stop buttons for stopwatch

--- countdown/timer.js
+++ countdown/timer.js
@@ -3,6 +3,12 @@ function getTime() {
     var hours = d.getHours();
     var minutes = d.getMinutes();
     var seconds = d.getSeconds();
+    if(d.getSeconds() < 10){
+        seconds = "0" + seconds;
+    }
+    if(d.getMinutes() < 10){
+        minutes = "0" + minutes;
+    }
     document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
     var t = setTimeout(getTime, 500);
 }
