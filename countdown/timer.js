5252bb3e8c27b90e71a06c49b7a2dff19da74f7d
Abby Bowerman <abbybowerman@outlook.com>
2020-08-07 14:05:51 -0400

    added some timer functionality

--- countdown/timer.js
+++ countdown/timer.js
@@ -1,3 +1,7 @@
+var start = document.getElementById("start");
+var stop = document.getElementById("stop");
+var timerHours = 0, timerMinutes = 0, timerSeconds = 0;
+
 function getTime() {
     var d = new Date();
     var hours = d.getHours();
@@ -11,4 +15,23 @@ function getTime() {
     }
     document.getElementById("time").innerHTML = hours + ":" + minutes + ":" + seconds;
     var t = setTimeout(getTime, 500);
+}
+
+function addSecond(){
+    timerSeconds++;
+    if(timerSeconds >= 60){
+        timerSeconds = 0;
+        timerMinutes++;
+    }
+    if(timerMinutes >= 60){
+        timerHours++;
+        timerMinutes = 0;
+    }
+    //DISPLAY TIME HERE
+    timer();
+}
+
+//This will cause the timer function to run every 1000 milliseconds (1 second)
+function timer() {
+    var t = setTimeout(addSecond, 1000);
 }
